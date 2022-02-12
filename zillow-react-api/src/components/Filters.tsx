731d0f7b76b1ba4cc9edbcf889ed27e20567fd80
Renato Dodani <renato.dodani1@gmail.com>
2022-02-11 21:00:59 -0500

    Added code for each filter and created filter components

---
+++ zillow-react-api/src/components/Filters.tsx
@@ -0,0 +1,33 @@
+import { useState } from "react";
+import { SearchByBaths } from "./SearchByBaths";
+import { SearchByBeds } from "./SearchByBeds";
+import { SearchByPrice } from "./SearchByPrice";
+import { SearchByPropertyType } from "./SearchByPropertyType";
+export function Filters() {
+
+
+    const [price, setPrice] = useState(0);
+    function setPriceValue(value: number) {
+        setPrice(value)
+    }
+    const [property, setProperty] = useState('');
+    function setPropertyType(value: string) {
+        setProperty(value)
+    }
+    const [beds, setBeds] = useState(0);
+    function setBedrooms(value: number) {
+        setBeds(value)
+    }
+    const [baths, setBaths] = useState(0);
+    function setBathrooms(value: number) {
+        setBaths(value)
+    }
+    return (
+        <div>
+            <SearchByPrice onSelect={(price) => setPriceValue(price)}></SearchByPrice>
+            <SearchByPropertyType onSelect={(property) => setPropertyType(property)}></SearchByPropertyType>
+            <SearchByBeds onSelect={(bed) => setBedrooms(bed)}></SearchByBeds>
+            <SearchByBaths onSelect={(bath) => setBathrooms(bath)}></SearchByBaths>
+        </div>
+    )
+}
