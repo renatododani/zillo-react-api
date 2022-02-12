53de492f07175d0d9139554894076144cc2b4ea5
Renato Dodani <renato.dodani1@gmail.com>
2022-02-11 20:59:37 -0500

    updated components

--- zillow-react-api/src/App.tsx
+++ zillow-react-api/src/App.tsx
@@ -1,24 +1,22 @@
 import React, { useEffect } from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import { realtyResponse } from "./services/Api";
-import { SearchByPrice } from "./components/SearchByPrice";
+import { RealtyResponse } from "./services/Api";
+// import { SearchByPrice } from "./components/SearchByPrice";
 import { SearchByPropertyType } from "./components/SearchByPropertyType";
 import { SearchByBaths } from "./components/SearchByBaths";
 import { SearchByBeds } from "./components/SearchByBeds";
 import { PropertySearch } from "./components/PropertySearch";
+import { Filters } from "./components/Filters";
 
 function App() {
   useEffect(() => {
-    realtyResponse();
+    RealtyResponse();
   }, []);
 
   return (
     <div className="App">
-      <SearchByPrice></SearchByPrice>
-      <SearchByPropertyType></SearchByPropertyType>
-      <SearchByBaths></SearchByBaths>
-      <SearchByBeds></SearchByBeds>
+      <Filters></Filters>
       <PropertySearch></PropertySearch>
     </div>
   );
