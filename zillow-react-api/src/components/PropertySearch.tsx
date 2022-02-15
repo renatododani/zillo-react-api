2b3b4c163fe4b5bed63ebcf31a42be715959104c
SudhaVallabhapurapu <89112048+sudhasew@users.noreply.github.com>
2022-02-14 22:08:36 -0500

    added routing

--- zillow-react-api/src/components/PropertySearch.tsx
+++ zillow-react-api/src/components/PropertySearch.tsx
@@ -1,19 +1,27 @@
 import { Filters } from "./Filters";
 import { getSearchDetails } from "../services/Api";
 import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { SearchResults } from "./SearchResults";
+
 export function PropertySearch() {
   const [searchTerm, setSearchTerm] = useState<any>("");
+  const navigate = useNavigate();
+  function onSubmit() {
+    navigate("/results");
+  }
 
   useEffect(() => {});
 
   return (
     <div>
-      <input type="text" placeholder="State, City,Zip" value={searchTerm} />
+      <input type="text" placeholder="State, City, Zip" value={searchTerm} />
       <span>
-        <button className="searchBtn" onClick={() => Filters()}>
+        <button className="searchBtn" onClick={onSubmit}>
           Search
         </button>
       </span>
+      {/* <Filters></Filters> */}
     </div>
   );
 }
--- zillow-react-api/src/components/SearchResults.tsx
+++ zillow-react-api/src/components/SearchResults.tsx
@@ -1,5 +1,12 @@
+import { useNavigate } from "react-router-dom";
+import { Filters } from "./Filters";
+
 export function SearchResults() {
-    return (
-        <div></div>
-    )
+  return (
+    <div>
+      <input type="text" placeholder="Search address" />
+      <button className="searchBtn">Search</button>
+      {/* <Filters></Filters> */}
+    </div>
+  );
 }
